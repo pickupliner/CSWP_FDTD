7592329fe1e83495714aa7c4985bcc43460d4e99
BuildTools <unconfigured@null.spigotmc.org>
2025-12-16 16:46:03 +0100

    Joran 16/12: grid refinement
    * Grid refinement rond de wedge
    * dx en dy zijn momenteel stap functies (zijn 2 keer zo fijn rond wedge)
    * Nog te doen: smooth interpolatie van dx en dy
    * instabiel: CN moet < 0.5
    * rectangle is broken: dx en dy in BC moeten nog geupdate worden

--- FDTD/__main__.py
+++ FDTD/__main__.py
@@ -42,38 +42,69 @@ plt.show()
 
 
 #source in function of time:
-Ps=lambda t:100*np.exp(-(t-3)**2)
+Ps = lambda t: np.exp(-(t - .5)**2*16) # source
 plt.plot(np.linspace(0,20,282),Ps(np.linspace(0,20,282)))
 plt.show()
 
-#spatial steps
-dx=0.1 #[m]
-dy=0.1 #[m]
-
-#time step based on CN and stabilaty
-dt=np.sqrt(2)*0.05   #[s]
-T=40 #time of simulation [s]
-
-#cournat number
-CN=c*dt/np.sqrt(dx**2+dy**2)  #[s/m]
-print(CN)
-
 #size of array
-K=int(T/dt) #dimensioneless
-N=int(a/dx) #dimensioneless
-M=int(b/dy) #dimensioneless
+K=int(565) #dimensioneless
+N=int(250) #dimensioneless
+M=int(150) #dimensioneless
 
 print(f"M={M}")
 print(f"N={N}")
 print(f"K={K}")
 
 
+def coordTransform(x, x0, x1, dx):
+    xM = np.max(x)
+    # xM = dx * (x1 - x0) + dx' * (xM - (x1 - x0))
+    # <-> xM - dx (x1 - x0) = dx' (xM - x1 + x0)
+    # <-> dx' = (xM - dx (x1 - x0)) / (xM - x1 + x0) 
+    dx_other = (xM - dx*(x1 - x0)) / (xM - x1 + x0)
+    u = np.where(x < x0, dx_other*x, np.where(x < x1, dx*(x - x0) + dx_other*x0, xM - dx_other*(xM - x)))
+    return u
+# discretised x-coords of o_x (for y-coord: use y_p)
+x_o = np.linspace(0, a, N+1) # [m]
+# change coordinates to get more points around wedge
+x_o = coordTransform(x_o, x0=7, x1=11, dx=0.5)
+plt.plot(x_o)
+plt.title("coordinate transformation")
+plt.show()
+# discretised y-coords of o_y (for x-coord: use x_p)
+y_o = np.linspace(0, b, M+1) # [m]
+y_o = coordTransform(y_o, x0=0, x1=6, dx=0.5)
+# discretised x- and y-coords of p
+x_p = (x_o[1:] + x_o[:-1])/2
+y_p = (y_o[1:] + y_o[:-1])/2
+
+# spatial step between x-coord of o_x
+dx_o = (x_o[1:] - x_o[:-1]).reshape((1,-1)) # [m]
+plt.plot(x_p, dx_o[0])
+plt.show()
+# spatial step between y-coord of o_y
+dy_o = (y_o[1:] - y_o[:-1]).reshape((-1,1)) # [m]
+# spatials steps between coords of p
+dx_p = (x_p[1:] - x_p[:-1]).reshape((1,-1)) # [m]
+dy_p = (y_p[1:] - y_p[:-1]).reshape((-1,1)) # [m]
+
+#time step based on CN and stabilaty
+# dt=np.sqrt(2)*0.05   #[s]
+dt = 0.5 * np.sqrt(np.min(dx_o)**2 + np.min(dy_o)**2) / c
+T=40 #time of simulation [s]
+
+# courant number
+CN=c*dt/np.sqrt(np.min(dx_o)**2+np.min(dy_o)**2) # [s/m]
+# since dx_p is mean of 2 neighbouring dx_o's it is always bigger than min of dx_o
+print(f"CN = {CN}")
+
+
 
 
 #constructing mesh
 ny, nx = M,N
-x = np.arange(0, nx) * dx  #(N,1) matrix
-y = np.arange(0, ny) * dy  #(M,1) matrix
+x = x_p  #(N,1) matrix
+y = y_p  #(M,1) matrix
 X, Y = np.meshgrid(x, y)   #(M,N) matrix both
 
 #kappa for x-direction
@@ -150,15 +181,15 @@ def empty(px,py,ox,oy,F1,F3,K,xs,ys,co):
         #construct total p
         p=px[i]+py[i]
         #two equations for o
-        ox[i+1,:,1:-1]=((1-k1_o*dt/2)/(1+k1_o*dt/2))*ox[i,:,1:-1]-dt/(dx*(1+k1_o*dt/2))*(p[:,1:]-p[:,:-1])
-        oy[i+1,1:-1,:]=((1-k2_o*dt/2)/(1+k2_o*dt/2))*oy[i,1:-1,:]-dt/(dy*(1+k2_o*dt/2))*(p[1:,:]-p[:-1,:])
+        ox[i+1,:,1:-1]=((1-k1_o*dt/2)/(1+k1_o*dt/2))*ox[i,:,1:-1]-dt/(dx_p*(1+k1_o*dt/2))*(p[:,1:]-p[:,:-1])
+        oy[i+1,1:-1,:]=((1-k2_o*dt/2)/(1+k2_o*dt/2))*oy[i,1:-1,:]-dt/(dy_p*(1+k2_o*dt/2))*(p[1:,:]-p[:-1,:])
 
         
         
 
         #two equations for p
-        px[i+1]=((1-k1_p*dt/2)/(1+k1_p*dt/2))*px[i]-(c**2/(1+k1_p*dt/2))*(dt/dx)*(ox[i+1,:,1:]-ox[i+1,:,:-1])
-        py[i+1]=((1-k2_p*dt/2)/(1+k2_p*dt/2))*py[i]-(c**2/(1+k2_p*dt/2))*(dt/dy)*(oy[i+1,1:,:]-oy[i+1,:-1,:])
+        px[i+1]=((1-k1_p*dt/2)/(1+k1_p*dt/2))*px[i]-(c**2/(1+k1_p*dt/2))*(dt/dx_o)*(ox[i+1,:,1:]-ox[i+1,:,:-1])
+        py[i+1]=((1-k2_p*dt/2)/(1+k2_p*dt/2))*py[i]-(c**2/(1+k2_p*dt/2))*(dt/dy_p)*(oy[i+1,1:,:]-oy[i+1,:-1,:])
 
         
 
@@ -203,16 +234,16 @@ def wall(px,py,ox,oy,F1,F2,K,xs,ys,co):
         #construct total p
         p=px[i]+py[i]
         #two equations for o
-        ox[i+1,:,1:-1]=((1-k1_o*dt/2)/(1+k1_o*dt/2))*ox[i,:,1:-1]-dt/(dx*(1+k1_o*dt/2))*(p[:,1:]-p[:,:-1])
-        oy[i+1,1:-1,:]=((1-k2_o*dt/2)/(1+k2_o*dt/2))*oy[i,1:-1,:]-dt/(dy*(1+k2_o*dt/2))*(p[1:,:]-p[:-1,:])
+        ox[i+1,:,1:-1]=((1-k1_o*dt/2)/(1+k1_o*dt/2))*ox[i,:,1:-1]-dt/(dx_p*(1+k1_o*dt/2))*(p[:,1:]-p[:,:-1])
+        oy[i+1,1:-1,:]=((1-k2_o*dt/2)/(1+k2_o*dt/2))*oy[i,1:-1,:]-dt/(dy_p*(1+k2_o*dt/2))*(p[1:,:]-p[:-1,:])
 
         #aplying boundry conditions
         ox[i+1,:ceiling,wall_left]=0
         
 
         #two equations for p
-        px[i+1]=((1-k1_p*dt/2)/(1+k1_p*dt/2))*px[i]-(c**2/(1+k1_p*dt/2))*(dt/dx)*(ox[i+1,:,1:]-ox[i+1,:,:-1])
-        py[i+1]=((1-k2_p*dt/2)/(1+k2_p*dt/2))*py[i]-(c**2/(1+k2_p*dt/2))*(dt/dy)*(oy[i+1,1:,:]-oy[i+1,:-1,:])
+        px[i+1]=((1-k1_p*dt/2)/(1+k1_p*dt/2))*px[i]-(c**2/(1+k1_p*dt/2))*(dt/dx_o)*(ox[i+1,:,1:]-ox[i+1,:,:-1])
+        py[i+1]=((1-k2_p*dt/2)/(1+k2_p*dt/2))*py[i]-(c**2/(1+k2_p*dt/2))*(dt/dy_p)*(oy[i+1,1:,:]-oy[i+1,:-1,:])
 
         
 
@@ -254,17 +285,17 @@ def rectangle(px,py,ox,oy,F1,F2,K,Z,xs,ys,co):
         #construct total p
         p=px[i]+py[i]
         #two equations for o
-        ox[i+1,:,1:-1]=((1-k1_o*dt/2)/(1+k1_o*dt/2))*ox[i,:,1:-1]-dt/(dx*(1+k1_o*dt/2))*(p[:,1:]-p[:,:-1])
-        oy[i+1,1:-1,:]=((1-k2_o*dt/2)/(1+k2_o*dt/2))*oy[i,1:-1,:]-dt/(dy*(1+k2_o*dt/2))*(p[1:,:]-p[:-1,:])
+        ox[i+1,:,1:-1]=((1-k1_o*dt/2)/(1+k1_o*dt/2))*ox[i,:,1:-1]-dt/(dx_p*(1+k1_o*dt/2))*(p[:,1:]-p[:,:-1])
+        oy[i+1,1:-1,:]=((1-k2_o*dt/2)/(1+k2_o*dt/2))*oy[i,1:-1,:]-dt/(dy_p*(1+k2_o*dt/2))*(p[1:,:]-p[:-1,:])
 
         #aplying boundry conditions
-        ox[i+1,:ceiling,wall_left]=((1-dt*Z/dx)*ox[i,:ceiling,wall_left]+2*dt/dx*p[:ceiling,wall_left-1])/(1+Z*dt/dx)
+        ox[i+1,:ceiling,wall_left]=((1-dt*Z/dx_o[:ceiling,wall_left])*ox[i,:ceiling,wall_left]+2*dt/dx_p[:ceiling,wall_left-1]*p[:ceiling,wall_left-1])/(1+Z*dt/dx)
         ox[i+1,:ceiling,wall_right]=((1-dt*Z/dx)*ox[i,:ceiling,wall_right]-2*dt/dx*p[:ceiling,wall_right])/(1+Z*dt/dx)
         oy[i+1,ceiling,wall_left:wall_right]=((1-dt*Z/dx)*oy[i,ceiling,wall_left:wall_right]-2*dt/dx*p[ceiling,wall_left:wall_right])/(1+Z*dt/dx)
 
         #two equations for p
-        px[i+1]=((1-k1_p*dt/2)/(1+k1_p*dt/2))*px[i]-(c**2/(1+k1_p*dt/2))*(dt/dx)*(ox[i+1,:,1:]-ox[i+1,:,:-1])
-        py[i+1]=((1-k2_p*dt/2)/(1+k2_p*dt/2))*py[i]-(c**2/(1+k2_p*dt/2))*(dt/dy)*(oy[i+1,1:,:]-oy[i+1,:-1,:])
+        px[i+1]=((1-k1_p*dt/2)/(1+k1_p*dt/2))*px[i]-(c**2/(1+k1_p*dt/2))*(dt/dx_o)*(ox[i+1,:,1:]-ox[i+1,:,:-1])
+        py[i+1]=((1-k2_p*dt/2)/(1+k2_p*dt/2))*py[i]-(c**2/(1+k2_p*dt/2))*(dt/dy_o)*(oy[i+1,1:,:]-oy[i+1,:-1,:])
 
         #removing inside obstacle ->not sure if neccecary
         px[i+1,:ceiling,wall_left:wall_right]=0
@@ -304,30 +335,24 @@ def triangle(px,py,ox,oy,F1,F2,K,xs,ys,co):
         # to be in triangle is the same as being beneath two lines
         return (y < upper_y*(x - left_x)) & (y < -upper_y * (x - right_x))
     
-    # discretisation points
-    x_ox = np.linspace(0, a, N+1).reshape((1,   N+1))
-    y_ox = np.linspace(0, b, M)  .reshape((M,   1))
-    x_oy = np.linspace(0, a, N)  .reshape((1,   N))
-    y_oy = np.linspace(0, b, M+1).reshape((M+1, 1))
-
-    TRIANGLE_ox = in_triangle(x_ox, y_ox)
-    TRIANGLE_oy = in_triangle(x_oy, y_oy)
+    TRIANGLE_ox = in_triangle(x_o.reshape((1, -1)), y_p.reshape((-1, 1)))
+    TRIANGLE_oy = in_triangle(x_p.reshape((1, -1)), y_o.reshape((-1, 1)))
 
     #solving scheme
     for i in range(K-1):
         #construct total p
         p=px[i]+py[i]
         #two equations for o
-        ox[i+1,:,1:-1]=((1-k1_o*dt/2)/(1+k1_o*dt/2))*ox[i,:,1:-1]-dt/(dx*(1+k1_o*dt/2))*(p[:,1:]-p[:,:-1])
-        oy[i+1,1:-1,:]=((1-k2_o*dt/2)/(1+k2_o*dt/2))*oy[i,1:-1,:]-dt/(dy*(1+k2_o*dt/2))*(p[1:,:]-p[:-1,:])
+        ox[i+1,:,1:-1]=((1-k1_o*dt/2)/(1+k1_o*dt/2))*ox[i,:,1:-1]-dt/(dx_p*(1+k1_o*dt/2))*(p[:,1:]-p[:,:-1])
+        oy[i+1,1:-1,:]=((1-k2_o*dt/2)/(1+k2_o*dt/2))*oy[i,1:-1,:]-dt/(dy_p*(1+k2_o*dt/2))*(p[1:,:]-p[:-1,:])
 
         #aplying boundry conditions
         ox[i+1,TRIANGLE_ox]=0
         oy[i+1,TRIANGLE_oy]=0
 
         #two equations for p
-        px[i+1]=((1-k1_p*dt/2)/(1+k1_p*dt/2))*px[i]-(c**2/(1+k1_p*dt/2))*(dt/dx)*(ox[i+1,:,1:]-ox[i+1,:,:-1])
-        py[i+1]=((1-k2_p*dt/2)/(1+k2_p*dt/2))*py[i]-(c**2/(1+k2_p*dt/2))*(dt/dy)*(oy[i+1,1:,:]-oy[i+1,:-1,:])
+        px[i+1]=((1-k1_p*dt/2)/(1+k1_p*dt/2))*px[i]-(c**2/(1+k1_p*dt/2))*(dt/dx_o)*(ox[i+1,:,1:]-ox[i+1,:,:-1])
+        py[i+1]=((1-k2_p*dt/2)/(1+k2_p*dt/2))*py[i]-(c**2/(1+k2_p*dt/2))*(dt/dy_o)*(oy[i+1,1:,:]-oy[i+1,:-1,:])
 
         # adding source
         px[i+1,xi,yi]+=Ps(i*dt)/2
@@ -371,14 +396,18 @@ def animate_heatmap(frames, interval=100, cmap='viridis', save_path=None, fps=10
 
     # Create figure
     fig, ax = plt.subplots()
-    heatmap = ax.imshow(frames[0], cmap=cmap, interpolation='nearest',origin='lower')   #,vmin=frames.min(), vmax=frames.max()/4)
-    plt.colorbar(heatmap, ax=ax)
+    cm = ax.pcolormesh(x_p, y_p, frames[0], shading='nearest')
+    # heatmap = ax.imshow(frames[0], cmap=cmap, interpolation='nearest',origin='lower')   #,vmin=frames.min(), vmax=frames.max()/4)
+    # plt.colorbar(heatmap, ax=ax)
+    plt.colorbar(cm, ax=ax)
 
     # Update function
     def update(frame_idx):
-        heatmap.set_data(frames[frame_idx])
+        # heatmap.set_data(frames[frame_idx])
+        cm.set_array(frames[frame_idx])
         ax.set_title(f"Frame {frame_idx}/{num_frames}")
-        return [heatmap]
+        # return [heatmap]
+        return [cm]
 
     # Create animation
     ani = animation.FuncAnimation(fig, update, frames=num_frames,
@@ -399,7 +428,7 @@ def animate_heatmap(frames, interval=100, cmap='viridis', save_path=None, fps=10
 
 animate_heatmap(p)
 
-t=np.linspace(0,T,int(T/dt)-1)
+t = dt*np.arange(0, K-1)
 
 plt.plot(t,obs[1])
 plt.show()
--- FDTD/version_historyFDTDmain.md
+++ FDTD/version_historyFDTDmain.md
@@ -10,3 +10,11 @@
 * Ik heb de wedge/triangle toegevoegd in __main__.py
 * Het is voor een harde wand (o_n = 0), Z=? was iets te ingewikkeld
 * Ik heb een nieuwe file non_cartesian.py gemaakt, hier is een niet orthogonaal grid gebruikt (mist nog absorberende randen) waarin de triangle vanzelfsprekender is
+
+## Joran 16/12
+
+* Grid refinement rond de wedge
+* dx en dy zijn momenteel stap functies (zijn 2 keer zo fijn rond wedge)
+* Nog te doen: smooth interpolatie van dx en dy
+* instabiel: CN moet < 0.5
+* rectangle is broken: dx en dy in BC moeten nog geupdate worden
