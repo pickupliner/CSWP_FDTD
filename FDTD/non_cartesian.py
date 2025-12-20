45e5a9db00cacf42e9871cc96d37a9cfd5ba67df
BuildTools <unconfigured@null.spigotmc.org>
2025-12-20 13:44:14 +0100

    Joran 20/12: non-cartesian orthogonal o
    * Made an o_w and o_v component, of which o_w is orthogonal to the triangle side
    * Reduced CN to 0.9 for stability
    * changed timespan c to T because of double usage (wavespeed)

--- FDTD/non_cartesian.py
+++ FDTD/non_cartesian.py
@@ -10,8 +10,10 @@ import matplotlib.pyplot as plt
 
 # Where normally one uses x and y direction orthogonal to eachother
 # Use here u and v which make an angle theta
-# e_u = e_x
-# e_v = e_x * cos theta + e_y * sin theta
+# e_u = e_x                                  e_x = e_u
+# e_v = e_x * cos theta + e_y * sin theta    e_y = (e_v - e_u * cos theta) / sin theta
+# introduce vector e_w which is orthogonal to e_v
+# e_w = e_x * sin theta - e_y * cos theta  
 theta = np.arctan(2 / (1/2)) # [rad]
 def to_xy(u, v):
     return u + v*np.cos(theta), v*np.sin(theta)
@@ -20,9 +22,9 @@ c = 1 # wavespeed [m/s]
 
 d = 6 # characteristic distance [m]
 
-us = d                                  # u-coordinate of source [m]
-vs = d/10                               # v-coordinate of source [m]
-Ps = lambda t: 4*np.exp(-(t - 0.3)**2*100) # source
+us = d                               # u-coordinate of source [m]
+vs = d/10                            # v-coordinate of source [m]
+Ps = lambda t: 4*np.exp(-(t - 3)**2) # source
 
 # returns True if x and y lie in the triangle
 def triangle(x, y):
@@ -33,72 +35,76 @@ def triangle(x, y):
 # Currently using a padding of d around important stuff (temporary)
 a = 5*d + 2*d                 # width in u-direction  [m]
 b = (2*d + 2*d)/np.sin(theta) # height in v-direction [m]
-c = 10                        # timespan              [s]
+T = b/c                       # timespan              [s]
 
 # Courant Number
-CN  = 1.0
+CN  = 0.9
 
-du = 0.2                                            # step in u-direction [m]
-dv = du/np.sin(theta)                               # step in v-direction [m]
-dt = 1/np.sqrt(1/du**2 + 1/(dv*np.sin(theta))**2)/c # timestep            [s]
+du = 0.2                                             # step in u-direction [m]
+dv = du/np.sin(theta)                                # step in v-direction [m]
+dt = CN/np.sqrt(1/du**2 + 1/(dv*np.sin(theta))**2)/c # timestep            [s]
 print(f"du x dv x dt = {du} x {dv} x {dt}")
 
 nu = int(np.ceil(a/du)) - 1 # size in u-direction [1]
 nv = int(np.ceil(b/dv)) - 1 # size in v-direction [1]
-nt = int(np.ceil(c/dt))     # size in time        [1]
+nt = int(np.ceil(T/dt))     # size in time        [1]
 print(f"nx x nv x nt = {nu} x {nv} x {nt}")
 
 p  = np.zeros((nu,     nv,     nt)) # p at discrete u, v and t points
-ox = np.zeros((nu + 1,     nv, nt)) # x component of o
-oy = np.zeros((nu, nv + 1,     nt)) # y component of o
+ow = np.zeros((nu + 1,     nv, nt)) # w component of o
+ov = np.zeros((nu, nv + 1,     nt)) # v component of o
 
 u_p = np.arange(du/2, a - du/2, du).reshape((nu, 1,  1))  # u-coordinates of discretized p field [m]
 v_p = np.arange(dv/2, b - dv/2, dv).reshape((1,  nv, 1))  # v-coordinates                        [m]
-t_p = np.arange(0, c, dt).reshape((1,  1,  nt))           # t-coordinates                        [s]
+t_p = np.arange(0, T, dt).reshape((1,  1,  nt))           # t-coordinates                        [s]
 
-u_ox = np.arange(0, a, du).reshape((nu + 1, 1, 1))
-v_ox = v_p
-t_ox = np.arange(dt/2, c + dt/2, dt).reshape((1, 1, nt))
+u_ow = np.arange(0, a, du).reshape((nu + 1, 1, 1))
+v_ow = v_p
+t_ow = np.arange(dt/2, T + dt/2, dt).reshape((1, 1, nt))
 
-u_oy = u_p
-v_oy = np.arange(0, b, dv).reshape((1, nv + 1, 1))
-t_oy = t_ox
+u_ov = u_p
+v_ov = np.arange(0, b, dv).reshape((1, nv + 1, 1))
+t_ov = t_ow
 
 plt.plot(t_p.reshape(-1), Ps(t_p.reshape(-1)))
 plt.title('source')
 plt.show()
 
-TRIANGLE_ox = triangle(*to_xy(u_ox, v_ox)).reshape((nu + 1, nv))
-TRIANGLE_oy = triangle(*to_xy(u_oy, v_oy)).reshape((nu,     nv + 1))
+TRIANGLE_ow = triangle(*to_xy(u_ow, v_ow)).reshape((nu + 1, nv))
+TRIANGLE_ov = triangle(*to_xy(u_ov, v_ov)).reshape((nu,     nv + 1))
 
 # scheme
 for l in range(1, nt):
-    dp_du = (p[1:,:,l-1] - p[:-1,:,l-1])/du
-    # e_v . nabla p = (e_x * cos theta + e_y * sin theta) . nabla = dp/dx cos theta + dp/dy sin theta
-    dp_dv = (p[:, 1:,l-1] - p[:,:-1,l-1])/dv
+    dp_du = (p[1:,:, l-1] - p[:-1,:,  l-1])/du
+    # e_v . nabla p = (e_x * cos theta + e_y * sin theta) . nabla p = dp/dx cos theta + dp/dy sin theta
+    dp_dv = (p[:, 1:,l-1] - p[:,  :-1,l-1])/dv
 
     dp_dx = dp_du
+
     tmp = np.gradient(p[:,:,l-1], axis=1) # dp/dv but at original points
-    dp_dy = (dp_dv - (tmp[:,1:] + tmp[:,:-1])/2*np.cos(theta))/np.sin(theta)
+    # e_w . nabla p = (e_x * sin theta - e_y * cos theta) . nabla p = dp/dx sin theta - dp/dy cos theta
+    #               = dp/dx sin theta - (dp/dv - dp/dx cos theta) cos theta / sin theta
+    #               = (dp/dx - dp/dv cos theta) / sin theta
+    dp_dw = (dp_dx - (tmp[1:,:] + tmp[:-1,:])/2*np.cos(theta)) / np.sin(theta)
 
     # update o
-    ox[1:-1,:,l] = ox[1:-1,:,l-1] - dt*dp_dx
-    oy[:,1:-1,l] = oy[:,1:-1,l-1] - dt*dp_dy
+    ow[1:-1,:,l] = ow[1:-1,:,l-1] - dt*dp_dw
+    ov[:,1:-1,l] = ov[:,1:-1,l-1] - dt*dp_dv
 
     # BC
-    ox[TRIANGLE_ox,l] = 0
-    oy[TRIANGLE_oy,l] = 0
+    ow[TRIANGLE_ow,l] = 0
 
-    dox_du = (ox[1:,:,l] - ox[:-1,:,l])/du
-    doy_du = (oy[1:,:,l] - oy[:-1,:,l])/du
-    doy_dv = (oy[:,1:,l] - oy[:,:-1,l])/dv
+    dow_du = (ow[1:,:,l] - ow[:-1,:,l])/du
+    dow_dv = (ow[:,1:,l] - ow[:,:-1,l])/dv
+    dov_dv = (ov[:,1:,l] - ov[:,:-1,l])/dv
 
-    dox_dx = dox_du
-    doy_dx = doy_du
-    tmp = np.gradient(oy[:,:,l], axis=0) # doy/du but at original points
-    doy_dy = (doy_dv - (tmp[:,1:] + tmp[:,:-1])/2*np.cos(theta))/np.sin(theta)
+    dow_dx = dow_du
 
-    div_o = dox_dx + doy_dy
+    tmp = np.gradient(ow[:,:,l], axis=1) # dow/dv but at original points
+    # e_w . nabla ow = (e_x * sin theta - e_y * cos theta) . nabla ow = dow/dx sin theta - dow/dy cos theta
+    dow_dw = (dow_dx - (tmp[1:,:] + tmp[:-1,:])/2*np.cos(theta)) / np.sin(theta)
+
+    div_o = dow_dw + dov_dv
 
     # update p
     p[:,:,l] = p[:,:,l-1] - c**2 * dt * div_o
