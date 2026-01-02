b0d1170621aa19bf24ae1b6b3b0bbf79244585af
pickupliner <115889469+pickupliner@users.noreply.github.com>
2026-01-02 17:29:49 +0100

    sam 02/01 anal?

--- FDTD/__main__.py
+++ FDTD/__main__.py
@@ -171,6 +171,63 @@ print(len(val),len(omega))
 plt.plot(2*np.pi*omega/c*distance,np.abs(val))
 
 plt.show()
+mask=omega >= 0.1
+omega=omega[mask]
+
+def F_UTD(x):
+    """
+    UTD Fresnel transition function F(x)
+
+    Parameters
+    ----------
+    x : array_like
+        Real, non-negative argument (k L A)
+
+    Returns
+    -------
+    F : complex ndarray
+        Fresnel transition function
+    """
+    x = np.asarray(x, dtype=float)
+
+    # Guard against tiny negative values from numerical noise
+    x = np.maximum(x, 0.0)
+
+    z = np.sqrt(2 / np.pi)*x
+
+    S_at_inf, C_at_inf = 0.5, 0.5
+    S, C = fresnel(np.sqrt(z))
+
+    # c = np.sqrt(-1j)
+    # at_inf = erf(c*np.inf)
+    # print(at_inf)
+    # at_sqrt_x = erf(c*np.sqrt(x))
+
+    return -2j * np.sqrt(x)*np.exp(-1j*x) * (2*(C_at_inf - C) + 2j*(S_at_inf - S))# np.sqrt(np.pi/4/c)*(at_inf - at_sqrt_x)
+    # return -2j * np.sqrt(x)*np.exp(-1j*x) np.sqrt(np.pi/4/c)*(at_inf - at_sqrt_x)
+
+def phid(a, b, thetad, thetar, thetas, n):
+    cot = lambda x:1/np.tan(x)
+    L = a*b/(a + b)*np.sin(thetad)**2
+    N_plus  = lambda alpha: np.round(-(np.pi - alpha)/(2*np.pi*n), decimals=0)
+    N_minus = lambda alpha: np.round( (np.pi + alpha)/(2*np.pi*n), decimals=0)
+    A_plus = lambda x: 2*np.cos((2*np.pi*n*N_plus(x)  - x)/2)**2
+    A_min  = lambda x: 2*np.cos((2*np.pi*n*N_minus(x) - x)/2)**2
+    alpha_plus  = thetar + thetas
+    alpha_minus = thetar - thetas
+    F = F_UTD
+    return  np.exp(1j*(2*np.pi*omega/c*(a+b)+np.pi/4))*(1/np.sin(thetad))*(1/np.sqrt(2*np.pi*2*np.pi*omega/c*a*b/(a+b)))*1/(2*n)*(cot((np.pi-alpha_minus)/(2*n))*F(omega*2*np.pi/c*L*A_min(alpha_minus))+cot((np.pi-alpha_plus)/(2*n))*F(omega*2*np.pi/c*L*A_min(alpha_plus))+cot((np.pi+alpha_plus)/(2*n))*F(omega*2*np.pi/c*L*A_plus(alpha_plus))+cot((np.pi+alpha_minus)/(2*n))*F(omega*2*np.pi/c*L*A_plus(alpha_minus)))
+plt.title("analitycal?")
+phi=phid(2.14*distance, 1.802*distance, 0.14*np.pi,0.187*np.pi, 0.147*np.pi, 2)+phid(2.32*distance, 1.802*distance, 0.141*np.pi,0.187*np.pi, 0.14*np.pi, 2)+phid(2.32*distance, 2.69*distance, 0.141*np.pi,0.12*np.pi, 0.14*np.pi, 2)+phid(2.14*distance, 2.69*distance, 0.147*np.pi,0.12*np.pi, 0.14*np.pi, 2)
+plt.plot(omega,np.abs(phi))
+plt.show()
+
+
+
+
+
+
+
 
 
 #constructing damping coëficcients for PML:
@@ -619,6 +676,8 @@ plt.show()
 
 P_w=np.fft.fft(obs[0],n=10*len(obs_empty[0]))
 P=np.fft.fft(obs_empty[0],n=10*len(obs_empty[0]))
+P_w=P_w[mask]
+P=P[mask]
 plt.title("triangle in comparison with empthy in ferquency")
 plt.xlabel("kd")
 plt.plot(2*np.pi*omega/c*distance,np.abs(P),label="empty")
@@ -627,54 +686,10 @@ plt.legend()
 plt.xlim(-0.1,10)
 plt.show()
 
-plt.plot(2*np.pi*omega/c*distance,np.abs(P_w)/np.abs(P))
-plt.ylim(0,1)
+plt.title("comparison")
+plt.plot(omega,np.abs(P_w)/np.abs(P),label="simulations")
+plt.plot(omega,phi,label="analytical")
+#plt.ylim(0,1)
 plt.xlim(0.1,10)
-plt.show()
-
-def F_UTD(x):
-    """
-    UTD Fresnel transition function F(x)
-
-    Parameters
-    ----------
-    x : array_like
-        Real, non-negative argument (k L A)
-
-    Returns
-    -------
-    F : complex ndarray
-        Fresnel transition function
-    """
-    x = np.asarray(x, dtype=float)
-
-    # Guard against tiny negative values from numerical noise
-    x = np.maximum(x, 0.0)
-
-    z = np.sqrt(2 / np.pi)*x
-
-    S_at_inf, C_at_inf = 0.5, 0.5
-    S, C = fresnel(np.sqrt(z))
-
-    # c = np.sqrt(-1j)
-    # at_inf = erf(c*np.inf)
-    # print(at_inf)
-    # at_sqrt_x = erf(c*np.sqrt(x))
-
-    return -2j * np.sqrt(x)*np.exp(-1j*x) * (2*(C_at_inf - C) + 2j*(S_at_inf - S))# np.sqrt(np.pi/4/c)*(at_inf - at_sqrt_x)
-    # return -2j * np.sqrt(x)*np.exp(-1j*x) np.sqrt(np.pi/4/c)*(at_inf - at_sqrt_x)
-
-def phid(a, b, thetad, thetar, thetas, n):
-    cot = lambda x:1/np.tan(x)
-    L = a*b/(a + b)*np.sin(thetad)**2
-    N_plus  = lambda alpha: np.round(-(np.pi - alpha)/(2*np.pi*n), decimals=0)
-    N_minus = lambda alpha: np.round( (np.pi + alpha)/(2*np.pi*n), decimals=0)
-    A_plus = lambda x: 2*np.cos((2*np.pi*n*N_plus(x)  - x)/2)**2
-    A_min  = lambda x: 2*np.cos((2*np.pi*n*N_minus(x) - x)/2)**2
-    alpha_plus  = thetar + thetas
-    alpha_minus = thetar - thetas
-    F = F_UTD
-    return np.exp(1j*(2*np.pi*omega/c*(a+b)+np.pi/4))*(1/np.sin(thetad))*(1/np.sqrt(2*np.pi*2*np.pi*omega/c*a*b/(a+b)))*1/(2*n)*(cot((np.pi-alpha_minus)/(2*n))*F(omega*2*np.pi/c*L*A_min(alpha_minus))+cot((np.pi-alpha_plus)/(2*n))*F(omega*2*np.pi/c*L*A_min(alpha_plus))+cot((np.pi+alpha_plus)/(2*n))*F(omega*2*np.pi/c*L*A_plus(alpha_plus))+cot((np.pi+alpha_minus)/(2*n))*F(omega*2*np.pi/c*L*A_plus(alpha_minus)))
-
-plt.plot(np.abs(phid(1, 2, np.pi, np.pi/3, 2*np.pi/3, 3/2)))
+plt.legend()
 plt.show()
